fd4650de2f01989b44af4c47ecc4361af2ebaed2
Sharat Mandava <sharatmandava@Sharats-MacBook-Air.local>
2025-12-23 08:22:21 -0600

    updated email issue

--- src/main/java/com/ksht/troop2605/scoutfinance/controller/MailTestController.java
+++ src/main/java/com/ksht/troop2605/scoutfinance/controller/MailTestController.java
@@ -7,8 +7,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import lombok.extern.slf4j.Slf4j;
+
 @RestController
 @RequestMapping("/api/test")
+@Slf4j
 public class MailTestController {
 
     @Autowired
@@ -17,11 +20,18 @@ public class MailTestController {
     @GetMapping("/email")
     public String sendTestEmail() {
         SimpleMailMessage message = new SimpleMailMessage();
-        message.setTo("YOUR_EMAIL@gmail.com"); // put YOUR email here
+        message.setTo("sharatmandava@hotmail.com"); // put YOUR email here
         message.setSubject("Scout Finance – Test Email");
         message.setText("If you received this, email is working!");
 
-        mailSender.send(message);
+        //mailSender.send(message);
+        try {
+            mailSender.send(message);
+            log.info("Email sent to {}", message.getTo()[0]);
+        } catch (Exception e) {
+            log.error("Email failed", e);
+        }
+
         return "Email sent!";
     }
 }
